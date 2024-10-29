9e41ce7951e237cdc626f720c56fd89bdbabadb3
prog-daiki <t.daiki0913@icloud.com>
2024-10-29 16:02:03 +0900

    courses pageの作成

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,25 @@
+// import { SearchInput } from "@/app/_components/common/search-input";
+// import { CategoryList } from "@/features/category/components/client/category-list";
+// import { CourseList } from "@/features/course/components/client/course-list";
+
+interface CoursesPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const CoursesPage = ({ searchParams }: CoursesPageProps) => {
+  return (
+    <div className="mt-4 space-y-2">
+      {/* <CategoryList />
+      <SearchInput /> */}
+      <div className="mt-4">
+        {/* <CourseList searchParams={searchParams} /> */}
+        test
+      </div>
+    </div>
+  );
+};
+
+export default CoursesPage;
