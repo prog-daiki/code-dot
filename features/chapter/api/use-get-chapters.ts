55904206b824b1e8b4ad97953da3922c1e3e840a
prog-daiki <t.daiki0913@icloud.com>
2024-10-30 17:07:28 +0900

    use-get-chaptersのリファクタリング

--- features/chapter/api/use-get-chapters.ts
+++ features/chapter/api/use-get-chapters.ts
@@ -1,12 +1,16 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
 
-import { Chapter } from "@/app/api/[[...route]]/core/chapter/types/chapter";
 import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.courses)[":course_id"]["chapters"]["$get"]
+>;
 
 export const useGetChapters = (
   courseId: string,
-): UseQueryResult<Chapter[], Error> => {
-  return useQuery<Chapter[], Error>({
+): UseQueryResult<ResponseType, Error> => {
+  return useQuery<ResponseType, Error>({
     queryKey: ["chapters", courseId],
     queryFn: async () => {
       const response = await client.api.courses[":course_id"].chapters.$get({
@@ -19,13 +23,7 @@ export const useGetChapters = (
         );
       }
 
-      const data = await response.json();
-      const chapters: Chapter[] = data.map((chapter) => ({
-        ...chapter,
-        createDate: new Date(chapter.createDate),
-        updateDate: new Date(chapter.updateDate),
-      }));
-      return chapters;
+      return await response.json();
     },
   });
 };
