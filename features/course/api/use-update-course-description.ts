aae3451012d53471bd8694f9ae9fde83f176c47d
prog-daiki <t.daiki0913@icloud.com>
2024-10-30 18:51:48 +0900

    use-update-course-descriptionの修正

--- features/course/api/use-update-course-description.ts
+++ features/course/api/use-update-course-description.ts
@@ -1,12 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { client } from "@/lib/hono";
-import { InferRequestType, InferResponseType } from "hono";
-
 import { toast } from "sonner";
+import { Course } from "@/app/api/[[...route]]/core/course/types/course";
 
-type RequestType = InferRequestType<(typeof client.api.courses)[":course_id"]["description"]["$put"]>["json"];
-type ResponseType = InferResponseType<(typeof client.api.courses)[":course_id"]["description"]["$put"]>;
+type RequestType = Pick<Course, "description">;
+type ResponseType = Course;
 
 export const useUpdateCourseDescription = (courseId: string) => {
   const queryClient = useQueryClient();
@@ -15,12 +14,17 @@ export const useUpdateCourseDescription = (courseId: string) => {
     mutationFn: async (json) => {
       const response = await client.api.courses[":course_id"].description.$put({
         param: { course_id: courseId },
-        json: { description: json.description },
+        json: { description: json.description ?? "" },
       });
       if (!response.ok) {
         throw new Error("講座の詳細の更新に失敗しました");
       }
-      return await response.json();
+      const data = await response.json();
+      return {
+        ...data,
+        createDate: new Date(data.createDate),
+        updateDate: new Date(data.updateDate),
+      };
     },
     onSuccess: (updatedCourse) => {
       toast.success("講座の詳細を更新しました");
