d27f0cf819f7b79fa44f3664a630da6f78d0ae34
prog-daiki <t.daiki0913@icloud.com>
2024-10-30 18:53:02 +0900

    use-update-course-titleの修正

--- features/course/api/use-update-course-title.ts
+++ features/course/api/use-update-course-title.ts
@@ -3,12 +3,9 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { client } from "@/lib/hono";
 import { Course } from "@/app/api/[[...route]]/core/course/types/course";
-import { InferResponseType } from "hono";
 
 type RequestType = Pick<Course, "title">;
-type ResponseType = InferResponseType<
-  (typeof client.api.courses)[":course_id"]["title"]["$put"]
->;
+type ResponseType = Course;
 
 export const useUpdateCourseTitle = (courseId: string) => {
   const queryClient = useQueryClient();
@@ -21,7 +18,12 @@ export const useUpdateCourseTitle = (courseId: string) => {
       if (!response.ok) {
         throw new Error("講座のタイトルの更新に失敗しました");
       }
-      return await response.json();
+      const data = await response.json();
+      return {
+        ...data,
+        createDate: new Date(data.createDate),
+        updateDate: new Date(data.updateDate),
+      };
     },
     onSuccess: (updatedCourse) => {
       toast.success("講座のタイトルを更新しました");
