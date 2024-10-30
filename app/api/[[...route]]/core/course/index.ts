5e4916039bd4973fd26e222a14647033e311cbee
prog-daiki <t.daiki0913@icloud.com>
2024-10-30 09:44:43 +0900

    講座価格編集APIの作成

--- app/api/[[...route]]/core/course/index.ts
+++ app/api/[[...route]]/core/course/index.ts
@@ -277,6 +277,39 @@ const Course = new Hono<{
         return HandleError(c, error, "講座サムネイル編集エラー");
       }
     },
+  )
+
+  /**
+   * 講座価格編集API
+   * @route PUT /api/courses/:course_id/price
+   * @middleware validateAdminMiddleware - 管理者権限の検証
+   * @returns 更新した講座
+   * @throws CourseNotFoundError
+   * @throws 講座価格編集エラー
+   */
+  .put(
+    "/:course_id/price",
+    validateAdminMiddleware,
+    zValidator("json", insertCourseSchema.pick({ price: true })),
+    zValidator("param", z.object({ course_id: z.string() })),
+    async (c) => {
+      const validatedData = c.req.valid("json");
+      const { course_id: courseId } = c.req.valid("param");
+      const courseUseCase = c.get("courseUseCase");
+      try {
+        const course: Course = await courseUseCase.updateCoursePrice(
+          courseId,
+          validatedData.price,
+        );
+        return c.json(course);
+      } catch (error) {
+        if (error instanceof CourseNotFoundError) {
+          console.error(`存在しない講座です: ID ${courseId}`);
+          return c.json({ error: Messages.MSG_ERR_003(Entity.COURSE) }, 404);
+        }
+        return HandleError(c, error, "講座価格編集エラー");
+      }
+    },
   );
 
 export default Course;
