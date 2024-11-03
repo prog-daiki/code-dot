8398c526d199ee4c0345339847c5f85556ed79b4
prog-daiki <t.daiki0913@icloud.com>
2024-11-03 16:04:21 +0900

    query-providerのリファクタリング

--- providers/query-provider.tsx
+++ providers/query-provider.tsx
@@ -1,17 +1,14 @@
-// In Next.js, this file would be called: app/providers.jsx
 "use client";
 
-// We can not useState or useRef in a server component, which is why we are
-// extracting this part out into it's own file with 'use client' on top
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const STALE_TIME = 60 * 1000;
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
+        staleTime: STALE_TIME,
       },
     },
   });
@@ -21,30 +18,19 @@ let browserQueryClient: QueryClient | undefined = undefined;
 
 function getQueryClient() {
   if (typeof window === "undefined") {
-    // Server: always make a new query client
     return makeQueryClient();
   } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
     if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
   }
 }
 
-type Props = {
+interface QueryProviderProps {
   children: React.ReactNode;
-};
+}
 
-export function QueryProvider({ children }: Props) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
+export function QueryProvider({ children }: QueryProviderProps) {
   const queryClient = getQueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
