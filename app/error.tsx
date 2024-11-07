44aecccf7187bc6e392bdcabe1aaa4e32547dc6b
prog-daiki <t.daiki0913@icloud.com>
2024-11-07 16:03:35 +0900

    エラー画面の作成

--- app/error.tsx
+++ app/error.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen p-4">
-      <Image src="/images/not-found.png" alt="404" width={500} height={500} />
+      <Image src="/images/error.png" alt="404" width={500} height={500} />
       <div className="flex flex-col items-center space-y-4">
-        <h1 className="text-md text-center">お探ししているページは見つかりませんでした。</h1>
+        <h1 className="text-md text-center">予期せぬエラーが発生しました。時間をおいて再度お試し下さい。</h1>
         <Link href="/">
           <Button variant="outline">トップページに戻る</Button>
         </Link>
